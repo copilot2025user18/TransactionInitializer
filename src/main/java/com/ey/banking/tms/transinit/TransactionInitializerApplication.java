e463b38185a0af6c731dd9997773eb7e89433128
labuser <labuser@ip-172-31-10-38.ap-south-1.compute.internal>
2025-04-19 07:11:27 +0000

    Initial commit

---
+++ src/main/java/com/ey/banking/tms/transinit/TransactionInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.ey.banking.tms.transinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionInitializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/banking/tms/transinit/TransactionInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.banking.tms.transinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
