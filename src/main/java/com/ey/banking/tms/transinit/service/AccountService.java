03eaf6ed39e409954b893a4360367748091644e9
jafar <jafar.kalam@in.ey.com>
2025-04-21 02:06:39 +0530

    Committing code

--- src/main/java/com/ey/banking/tms/transinit/service/AccountService.java
+++ src/main/java/com/ey/banking/tms/transinit/service/AccountService.java
@@ -3,18 +3,25 @@ package com.ey.banking.tms.transinit.service;
 import com.ey.banking.tms.transinit.model.Account;
 import com.ey.banking.tms.transinit.model.BenefAccount;
 import com.ey.banking.tms.transinit.repo.BenefAccountRepo;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cache;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
+
 @Service
 public class AccountService {
 
+    private final WebClient balanceServiceWebClient = WebClient.builder().baseUrl("http://balance-service").build();;
+
+
     BenefAccountRepo benefitAccountRepo;
+
 
     public Account getAccountByAccountId(Long accountId){
         Account account = new Account();
@@ -34,11 +41,31 @@ public class AccountService {
         return optional.orElseGet(() -> benefitAccountRepo.save(benefetryAccount));
     }
 
+
     public Boolean checkBalenceForTrans(BigDecimal amount, Account sourceAccount) {
-        return true;
+
+        return balanceServiceWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/balance/check")
+                        .queryParam("accountId", sourceAccount.getAccountId())
+                        .queryParam("amount", amount.longValue())
+                        .build())
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .onErrorReturn(false)
+                .block();
     }
 
     public Boolean checkTransLimitExausted(BigDecimal amount, Account sourceAccount) {
-        return true;
+        return balanceServiceWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/limit/check")
+                        .queryParam("accountId", sourceAccount.getAccountId())
+                        .queryParam("amount", amount.longValue())
+                        .build())
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .onErrorReturn(false)
+                .block();
     }
 }
